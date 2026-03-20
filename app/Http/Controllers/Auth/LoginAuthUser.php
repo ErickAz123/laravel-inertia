b89de456097a74d39a5382e4d42d8952a2c68a56
ErickAz123 <erickalaziz17@gmail.com>
2026-03-20 08:09:26 +0700

    20/03/2026 08:10

--- app/Http/Controllers/Auth/LoginAuthUser.php
+++ app/Http/Controllers/Auth/LoginAuthUser.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Request\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -15,12 +17,24 @@ class LoginAuthUser extends Controller
         return Inertia::render('auth/Login');
     }
 
-    public function store(LoginRe $request): RedirectResponse
+    public function store(LoginRequest $request): RedirectResponse
     {
         $request->authecticate();
 
         $request->sessions()->regenerate();
 
+        return redirect()->intended(route('dashboard', absolute: false));
 
+    }
+
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
---
+++ app/Http/Controllers/Request/LoginRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Request;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+
+class LoginRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'username' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    public function authenticated(): void
+    {
+        $this->ensureIsNotLimited();
+
+        if (! Auth::attempt($this->only('username', 'password'), $this->boolean('remember'))) {
+            RateLimiter::hit($this->throtleKey());
+
+            throw ValidationException::withMessages([
+                'username' => trans('auth.failed'),
+            ]);
+        }
+        RateLimiter::clear($this->throtleKey());
+    }
+
+    public function ensureIsNotRateLimited(): void
+    {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
+
+        event(new Lockout($this));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
+
+    /**
+     * Get the rate limiting throttle key for the request.
+     */
+    public function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->string('email')).'|'.$this->ip());
+    }
+}
